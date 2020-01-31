9e647ab2d3a5863c1b62e19a57d5494c75e3e8ae
hazy <gracrys@gmail.com>
2020-01-30 22:58:41 -0430

    effect in link hover

--- src/components/views/Home.svelte
+++ src/components/views/Home.svelte
@@ -18,6 +18,27 @@ import RouterLink from '../../router/RouterLink.svelte';
  h3,h4{
   display: inline;
   margin: auto;
+  a{
+    position: relative;
+    &::before{
+      height: 0;  
+      transition: height 0.6s;
+      content: "";
+      visibility: hidden;
+    }
+    &:hover::before{
+        visibility: visible;
+        background-color: #ffffff88;
+        content: "";
+        height: 1.2rem;
+        position: absolute;
+        left: 0;
+        bottom: 8px;
+        width: 100%;
+        z-index: -1;
+
+    }
+  }
  }
  h3{
   display: block
