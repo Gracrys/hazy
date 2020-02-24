141957af86871b16d345c71854bc2ecee4084694
Gracrys <gracrys@gmail.com>
2020-02-23 19:37:16 -0430

    updated some about me section

---
+++ src/components/views/About.svelte
@@ -0,0 +1,42 @@
+<script>
+    import feather from "~/../assets/feather.png"
+</script>
+<style lang="scss">
+
+section{
+    display: flex;
+    align-items: center;
+    @media (max-width: 900px) {
+        flex-wrap: wrap;
+    }
+    article{
+        margin-right: 10%;
+        img{
+            width: 100%;
+        }
+    }
+    address{
+        text-align: center;
+    }
+}
+
+</style>
+<section class="container mt-4" >
+    <article>
+        <img src={feather} alt="feather">
+        <address class="mt-5">
+            <i>
+                You can contact me at gracrys@gmail.com
+            </i>    
+        </address>
+    </article>
+    <div>
+        <b><i>
+        « However i continue to try and i continue, indefatigably, to reach out. There's no way i can single-handedly save the world or, perhaps, even make a perceptible difference - but how ashamed i would be to let a day pass withouyt making one more effort.»
+            -Isaac Asimov
+        </i></b>
+        <p>
+        
+        </p>
+    </div>
+</section>
